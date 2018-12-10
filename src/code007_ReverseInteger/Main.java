220f61afd89f56fcea9ee95b89dc4d16438b6ec2
ZhangDan062 <1071102546@qq.com>
2018-12-10 23:04:40 +0800

    反转整数；BigInteger使用

---
+++ src/code007_ReverseInteger/Main.java
@@ -0,0 +1,26 @@
+package code007_ReverseInteger;
+
+public class Main {
+	public static int reverse(int x) {
+		int rs=0;
+		while (x != 0) {
+			//从个位依次得到数值
+			int num=x % 10;
+			x = x / 10;
+			//从左到右运算符 
+			if (rs>Integer.MAX_VALUE/10||(rs==Integer.MAX_VALUE/10&&num>7)) {
+				return 0;
+			}
+			if (rs<Integer.MIN_VALUE/10||(rs==Integer.MIN_VALUE/10&&num<-8)) {
+				return 0;
+			}
+			//这样求和更为简单 【相比于原数逆序输入到集合，集合从最后再求和】
+			//边获取原数的个位数，边求和【而不是全部逆序后，边获取逆序数的个位数，边求和】
+			rs=rs*10+num;
+		}
+		return 0;
+	}
+	public static void main(String[] args) {
+		reverse(1463847412);
+	}
+}
