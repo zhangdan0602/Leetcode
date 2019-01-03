d911949a12512ad316b83ad0abb9363abad37916
ZhangDan062 <1071102546@qq.com>
2019-01-03 20:48:00 +0800

    java doc

---
+++ src/Common/TestDoc.java
@@ -0,0 +1,22 @@
+package Common;
+
+/**
+ * @author:zd
+ * @date:2019年1月3日下午4:46:16
+ * @parameter
+ * @return
+ */
+public class TestDoc {
+	/**
+	 * @author : suyuyuan
+	 * @date 创建时间：${date} ${time}
+	 * @version 1.0
+	 * @parameter
+	 * @since
+	 * @return
+	 * 
+	 */
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/Common/docInterpretation.java
@@ -0,0 +1,44 @@
+package Common;
+
+/**
+ * @author Zd
+ *
+ */
+
+//alt+command+J生成全类的doc注释；选中方法按快捷键是方法的doc注释；
+//windows-->preference-->Java-->Code Style-->Code Templates-->code-->new Java file
+/** 
+* @author : suyuyuan
+* @date 创建时间：${date} ${time} 
+* @version 1.0 
+* @parameter  
+* @since  
+* @return 
+* @see 
+* @exception
+*/
+public class docInterpretation {
+
+	
+	/**
+	 * @param s
+	 * @return
+	 */
+	public static int  getNum(int s) {
+		return 0;
+	}
+	
+	/**
+	 * @param s
+	 * @return
+	 */
+	public static int  setNum(int s) {
+		return 0;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+}
