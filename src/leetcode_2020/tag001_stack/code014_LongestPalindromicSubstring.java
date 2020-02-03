dc0bfe8a458d5b85d0ea6d0822659ce01492a316
zd <1071102546@qq.com>
2020-02-03 10:04:10 +0800

    最长回文、z字形转换
    中间扩散、flag+-

---
+++ src/leetcode_2020/tag001_stack/code014_LongestPalindromicSubstring.java
@@ -0,0 +1,34 @@
+package leetcode_2020.tag001_stack;
+
+public class code014_LongestPalindromicSubstring
+{
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() == 0)
+            return "";
+        String rs = s.substring(0,1);
+        int maxLen = 1;
+        for (int i = 0; i < s.length(); i++)
+        {
+            String len1 = expandAroundCentre(s, i, i);
+            String len2 = expandAroundCentre(s, i, i + 1);
+            String len = len1.length() > len2.length()? len1 : len2;
+            if (len.length() > maxLen){
+                maxLen = len.length();
+                rs = len;
+            }
+        }
+        return rs;
+    }
+    public static String expandAroundCentre(String s, int start, int end){
+        int l = start, r = end;
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+            l--;
+            r++;
+        }
+        return s.substring(l + 1, r);
+    }
+    public static void main(String agrs[]){
+        System.out.println(longestPalindrome("babad"));
+    }
+
+}
---
+++ src/leetcode_2020/tag001_stack/code015_ZigZagConversion.java
@@ -0,0 +1,38 @@
+package leetcode_2020.tag001_stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class code015_ZigZagConversion
+{
+    public static String convert(String s, int numRows) {
+        if (numRows == 1)
+            return s;
+        List<StringBuilder> list = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+        {
+            list.add(new StringBuilder());
+        }
+        int curRow = 0;
+        //boolean goingDown = false;
+        int goingDown = -1;
+        for (char c: s.toCharArray())
+        {
+            list.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1)
+                //goingDown = !goingDown;
+                goingDown = - goingDown;
+            //curRow += goingDown ? 1 : -1;
+            curRow += goingDown;
+        }
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder sb : list)
+        {
+            res.append(sb);
+        }
+        return res.toString();
+    }
+    public static void main(String args[]){
+        System.out.println(convert("LEETCODEISHIRING", 3));
+    }
+}
